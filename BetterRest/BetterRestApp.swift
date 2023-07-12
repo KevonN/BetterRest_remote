675f4031a550b997f5f7dc78c067dbe8eafed6b4
Kevin Nguyen <kevinvietnguyen2003@gmail.com>
2023-07-12 14:38:37 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kevin Nguyen on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
